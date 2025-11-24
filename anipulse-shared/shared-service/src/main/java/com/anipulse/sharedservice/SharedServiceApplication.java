977a3d8e41627f1f40c7865b8c7db8550bee9f79
lee-sese <coulstonesese04@gmail.com>
2025-11-24 09:16:19 +0300

    config: add shared library

---
+++ anipulse-shared/shared-service/src/main/java/com/anipulse/sharedservice/SharedServiceApplication.java
@@ -0,0 +1,12 @@
+package com.anipulse.sharedservice;
+
+/**
+ * Shared Service Library Module
+ * This module provides common security utilities and converters
+ * for all Anipulse microservices.
+ *
+ * This is NOT a runnable application - it's a library dependency.
+ */
+public class SharedServiceApplication {
+    // This is a library module - no main method needed
+}
---
+++ anipulse-shared/shared-service/src/main/java/com/anipulse/sharedservice/security/SecurityConstants.java
@@ -0,0 +1,13 @@
+package com.anipulse.sharedservice.security;
+
+public class SecurityConstants {
+    public static final String ROLE_USER = "ROLE_USER";
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
+    public static final String ROLE_MODERATOR = "ROLE_MODERATOR";
+
+    public static final String USER_ID_CLAIM = "sub";
+    public static final String USERNAME_CLAIM = "preferred_username";
+    public static final String EMAIL_CLAIM = "email";
+
+
+}
